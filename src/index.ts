cc31f90e6804decee1637e71d391d86309ef4ef5
YUN <yunmyeonghun@gmail.com>
2025-07-27 16:12:36 +0900

    basic data type

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,22 @@
+// number type
 let age: number = 20;
-
 if (age < 50) {
   age += 10;
 }
-
 console.log(age);
+
+// array of numbers
+let ages: number[] = [20, 30, 40];
+ages.forEach((age) => {
+  if (age < 50) {
+    age += 10;
+  }
+  console.log(age);
+});
+
+// tuple type
+let person: [string, number] = ['yunmy', 43];
+
+// enum type
+enum Size { Small = 1, Medium, Large }
+let mySize: Size = Size.Medium;
